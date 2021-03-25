022c13af730c3caba9b48af75380cb1099dd5bbe
Yudi <madewahyudi22@gmail.com>
2021-03-25 14:01:31 +0800

    update web: pramayasa

--- web/pramayasa/setup.sh
+++ web/pramayasa/setup.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
+#!/bin/env bash
 
-NAME="web_pramayasa"
-PORT="3001"
 PHP_INI_DIR="/usr/local/etc/php"
 
-env PORT=$PORT docker-compose up -d --build --force-recreate
+docker-compose up -d --build --force-recreate
 docker-compose exec php cp "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini"
