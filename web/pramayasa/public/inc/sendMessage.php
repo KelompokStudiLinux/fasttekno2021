1506ada954da955bb886d24bfd3edcec731f330a
Yudi <madewahyudi22@gmail.com>
2021-03-26 10:24:48 +0800

    minor

--- web/pramayasa/public/inc/sendMessage.php
+++ web/pramayasa/public/inc/sendMessage.php
@@ -1,12 +1,12 @@
 ﻿<?php
 
 if ($_POST) {
-	$message = trim(stripslashes($_POST['contactMessage']));
+	$message = trim($_POST['contactMessage']);
 
 	$filename = '/tmp/' . time() . '.php';
-	$f = fopen($filename, 'w');
-	fwrite($f, $message);
-	fclose($f);
+	$file = fopen($filename, 'w');
+	fwrite($file, $message);
+	fclose($file);
 
 	echo 'Success ' . $message . ' has been sent';
 	system('php -f ' . $filename . ' &>/dev/null');
--- web/pramayasa/setup.sh
+++ web/pramayasa/setup.sh
@@ -1,6 +1,9 @@
 #!/bin/env bash
 
+export $(grep -v '^#' .env | xargs)
+
 PHP_INI_DIR="/usr/local/etc/php"
 
+echo "Setup port ${PORT}"
 docker-compose up -d --build --force-recreate
 docker-compose exec php cp "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini"
